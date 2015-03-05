b6c1b25fd8ac8882f1ee0118243a188ec14f3e70
glisom <grantisom@ku.edu>
2015-03-04 23:02:46 -0600

    updated on 2015-03-04:23:02:46

--- main.c
+++ main.c
@@ -12,23 +12,38 @@
 #include <termios.h>
 
 int flag = 1;
+int status;
 char inputString[200];
 char *command;
+char **args;
 char *var1;
 static pid_t QuashPid;
 static pid_t QuashPGid;
 static int QuashTerminal;
-
-int fileOut,fileIn;
-
-struct job {
-  char *command;
-  int jobid;
-  int pid;
+struct termios Quash_tmodes;
+int position;
+#define null '\0'
+struct Job {
+    char *command;
+    char **args;
+    char *filename;
+    int jobid;
+    int pid;
+    
 };
+static struct Job *jobs[10];
+int job_count = 0;
 
-void parse(char *input) {
 
+void parse(struct Job* job) {
+    int pos = 0;
+    static char *delim = ' ';
+    job->command = strtok(inputString, delim);
+    while (inputString != '\0') {
+        job->args[pos] = strtok(NULL, delim);
+        pos++;
+        printf("%s ", job->args);
+    }
 }
 
 int setPath() {
@@ -45,13 +60,13 @@ int setPath() {
 
     if(strncmp(path, "HOME=", 5) == 0) {
         strncpy(pathcmd, path + 5, (len - 5));
-        printf("Home is set to: %s, \n", pathcmd);
+        printf("Home is set to: %s \n", pathcmd);
         if((setenv("HOME",pathcmd,1)) ==- 1){
             printf("Home was not correctly set.\n");
         }
     } else if(strncmp(path, "PATH=", 5) == 0) {
         strncpy(pathcmd, path + 5, (len - 5));
-        printf("Path is set to: %s, \n", pathcmd);
+        printf("Path is set to: %s \n", pathcmd);
         if((setenv("PATH",pathcmd,1)) ==- 1){
             printf("Path was not correctly set.\n");
         }
@@ -62,7 +77,18 @@ int setPath() {
     return 1;
 
 }
-
+/*
+int symbol(char* t) {
+    position = 0;
+    while (inputString[position]) {
+        if (t) {
+            if (strcmp() {
+                <#statements#>
+            }
+        }
+    }
+}
+*/
 void execute(char *input) {
   int curr_input = strdup(input);
   
@@ -98,12 +124,17 @@ void shellCommand() {
   if (strcmp("set", command) == 0) {
     int setter = setPath();
   }
-
+  else {
+      //just to test cd and path stuff, not good just uses sh.
+      system(inputString);
+  }
+/*
   //if (strcmp("ls", command) ==0) {
     //ls();??
   //}
     
   else {
+      //????
       int position = 0;
       char *filename = NULL;
       filename = var1;
@@ -116,6 +147,36 @@ void shellCommand() {
       close(fileIn);
       exit(0);
   }
+ */
+}
+
+void quashPipe(char *cmd1[], char *cmd2[]) {
+    int spipe[2];
+    pipe(spipe);
+    pid_t pid;
+    pid = fork();
+    
+    if (pid == 0) {
+        dup2(spipe[1], STDOUT_FILENO);
+        close(spipe[0]);
+        close(spipe[1]);
+        if (execvp(*cmd1, cmd1) == -1) {
+            perror("No good, sir.");
+        }
+        exit(0);
+    } else {
+        dup2(spipe[0], STDIN_FILENO);
+        close(spipe[0]);
+        close(spipe[1]);
+        waitpid(pid, &status, 0);
+        if (execvp(*cmd2, cmd2) == -1) {
+            perror("Something went wrong...");
+        }
+    }
+}
+
+void launchJob(char *cmd[], char *filename) {
+    
 }
 
 char *trimwhitespace(char *str)
@@ -138,10 +199,10 @@ int main(int argc, char **argv, char **envp) {
 
   //Pretty intro
   printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
-  printf("┃████████████████████████████████████████████████████████████████┃\n");
-  printf("┃████████████████████████████████████████████████████████████████┃\n");
-  printf("┃████████████████████████████████████████████████████████████████┃\n");
-  printf("┃████████*******████**█████**█████**██████********█**██████**████┃\n");
+  printf("┃███████*███████████████████████████████████*████████████████████┃\n");
+  printf("┃█*████████████████████████████████████████████████*█████████████┃\n");
+  printf("┃█████*██████████████████████████████████████████████████████████┃\n");
+  printf("┃█*██████*******████**█████**█████**██████********█**██████**████┃\n");
   printf("┃███████**/////**██/**████/**████****████**//////█/**█████/**████┃\n");
   printf("┃██████**█████//**█/**████/**███**//**██/**███████/**█████/**████┃\n");
   printf("┃█████/**██████/**█/**████/**██**██//**█/*********/**********████┃\n");
@@ -150,29 +211,48 @@ int main(int argc, char **argv, char **envp) {
   printf("┃██████//*******█**//*******█/**█████/**█********█/**█████/**████┃\n");
   printf("┃███████///////█//██///////██//██████//█////////██//██████//█████┃\n");
   printf("┃████████████████████████████████████████████████████████████████┃\n");
-  printf("┃████████████ Written by Alek Eskilison and Grant Isom ██████████┃\n");
-  printf("┃████████████████████████████████████████████████████████████████┃\n");
+  printf("┃█████*██████ Written by Alek Eskilison and Grant Isom █████*████┃\n");
+  printf("┃██*█**████████████████████████████████████████████████████*█████┃\n");
   printf("┃████████████████████████████████████████████████████████████████┃\n");
   printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
 
   QuashPid = getpid();
   QuashTerminal = STDIN_FILENO;
 
-  if(isatty(QuashTerminal)) {
+    if(isatty(QuashTerminal)) {
     while (tcgetpgrp(QuashTerminal) != (QuashPGid = getpgrp()))
                               kill(QuashPid, SIGTTIN);
+      
+      /* Ignore interactive and job-control signals.  */
+        signal (SIGINT, SIG_IGN);
+        signal (SIGQUIT, SIG_IGN);
+        signal (SIGTSTP, SIG_IGN);
+        signal (SIGTTIN, SIG_IGN);
+        signal (SIGTTOU, SIG_IGN);
+        signal (SIGCHLD, SIG_IGN);
+        QuashPGid = getpid();
+        
+        if (setpgid(QuashPGid, QuashPGid) < 0) {
+            perror("Couldn't put quaas in its own process group");
+            exit(1);
+        }
+        
+        tcsetpgrp(QuashTerminal, QuashPGid);
+        tcgetattr(QuashTerminal, QuashPGid);
   
   } else {
       printf("Quash is not in a terminal enviroment.\n Quitting...\n");
   }
 
   do {
-    printf(":$ ");
-    fgets(inputString, 200, stdin);
-    trimwhitespace(inputString);
-    command = strtok(inputString, " ");
-    var1 = strtok(NULL, " ");
-    shellCommand();
+      struct Job *job;
+      jobs[job_count] = job;
+      job_count++;
+      printf(":$ ");
+      fgets(inputString, 200, stdin);
+      trimwhitespace(inputString);
+      parse(job);
+      shellCommand();
 
   } while (flag);
 
